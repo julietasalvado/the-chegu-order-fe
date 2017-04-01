e0e5294783b5f3886fd0cb72a5f5ac38c6fa13fc
Julieta Salvadó <julieta.salvado@gmail.com>
2017-04-01 03:30:28 -0300

    Initial routing and homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-/**
- * Created by Julieta Salvado.
- */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-console.log("hola");
+render(
+  <Router history={browserHistory} routes={routes}/>,
+  document.getElementById('app')
+);
 
