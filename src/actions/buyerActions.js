a6d2bd0b6d4059107e81076722f94d9abb361043
Julieta Salvadó <julieta.salvado@gmail.com>
2017-04-08 18:36:10 -0300

    Delete unused code

--- src/actions/buyerActions.js
+++ src/actions/buyerActions.js
@@ -1,13 +1,6 @@
 import * as types from './actionTypes';
 import buyerApi from '../api/mockUserApi';
 
-/*export function addBuyer(user) {
-  return {
-    type: types.ADD_BUYER,
-    user /!*this is the same that user: user*!/
-  };
-}*/
-
 export function loadBuyersSuccess(users) {
   return {
     type: types.LOAD_BUYERS_SUCCESS,
