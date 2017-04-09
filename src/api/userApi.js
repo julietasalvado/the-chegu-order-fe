1d1d8858e855a8e826c4f85cc5199107eeb0e810
Julieta Salvadó <julieta.salvado@gmail.com>
2017-04-09 20:07:54 -0300

    Add actual saving feature

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,16 +1,4 @@
-// This file mocks a web API by working with the hard-coded data below.
-// It uses setTimeout to simulate the delay of an AJAX call.
-// All calls return promises.
-
 import React from 'react';
-import delay from './delay';
-
-let users = [];
-
-//This would be performed on the server in a real app. Just stubbing in.
-const generateId = () => {
-  return users.length + 1;
-};
 
 // var item_url = `{API_URL}/items`;
 let item_url = `http://localhost:8080/api/v1/items`;
@@ -25,12 +13,6 @@ class UserApi {
   }
 
   static getAllBuyers() {
-    // return new Promise((resolve, reject) => {
-    //   setTimeout(() => {
-    //     resolve(Object.assign([], users));
-    //   }, delay);
-    // });
-
     const headers = this.requestHeaders();
     // var obj = {
     //   method: 'POST',
@@ -53,31 +35,44 @@ class UserApi {
     });
   }
 
-  static
-  saveBuyer(buyer) {
+  static saveBuyer(buyer) {
     buyer = Object.assign({}, buyer); // to avoid manipulating object passed in.
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        // Simulate server-side validation
-        const minAuthorNameLength = 3;
-        if (buyer.username.length < minAuthorNameLength) {
-          reject(`Username must be at least ${minAuthorNameLength} characters.`);
-        }
 
-        if (buyer.id) {
-          const existingAuthorIndex = users.findIndex(a => a.id == buyer.id);
-          users.splice(existingAuthorIndex, 1, buyer);
-        } else {
-          //Just simulating creation here.
-          //The server would generate ids for new authors in a real app.
-          //Cloning so copy returned is passed by value rather than by reference.
-          buyer.id = generateId();
-          users.push(buyer);
-        }
+    const headers = this.requestHeaders();
+    let obj = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(buyer)
+    };
 
-        resolve(buyer);
-      }, delay);
+    return fetch(item_url, obj).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
     });
+
+    // return new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     // Simulate server-side validation
+    //     const minAuthorNameLength = 3;
+    //     if (buyer.username.length < minAuthorNameLength) {
+    //       reject(`Username must be at least ${minAuthorNameLength} characters.`);
+    //     }
+    //
+    //     if (buyer.id) {
+    //       const existingAuthorIndex = users.findIndex(a => a.id == buyer.id);
+    //       users.splice(existingAuthorIndex, 1, buyer);
+    //     } else {
+    //       //Just simulating creation here.
+    //       //The server would generate ids for new authors in a real app.
+    //       //Cloning so copy returned is passed by value rather than by reference.
+    //       buyer.id = generateId();
+    //       users.push(buyer);
+    //     }
+    //
+    //     resolve(buyer);
+    //   }, delay);
+    // });
   }
 }
 
